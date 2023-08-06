73f2ae0ff31856e37dc379ad7afe5bb15f1fe7dd
BrunoRamos88 <88brunoramos@gmail.com>
2023-08-05 21:25:42 -0300

    Criação dos componentes Button e Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "./styles/globals"
 import { theme } from "./styles/theme"
+import { Home } from "./pages/Home"
+import { Header } from "./components/Header"
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Header />
+      <Home />
       <h1>The Movie DB</h1>
     </ThemeProvider>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { FaBars, FaHome } from "react-icons/fa"
+import Logo from "../../assets/logo.png"
+import { Button } from "../Button"
+import * as Styles from "./styles"
+
+export function Header() {
+  return (
+    <Styles.Container>
+      <div>
+          <img
+            src={Logo}
+            alt="Logo de The Movie DB. Botão de play ao lado do texto 'The Movie DB'"
+          />
+
+          <FaHome />
+          Home
+          <FaBars />
+          Minha lista
+      </div>
+
+      <form className="search-wrapper">
+        <input
+          type="text"
+          placeholder="Pesquise um filme"
+        />
+
+        <Button style={{ borderRadius: '0 4px 4px 0' }} type="submit">Pesquisar</Button>
+      </form>
+    </Styles.Container>
+  )
+}
