1d3559e5126383803d1ed9a3ab139b153f068d84
BrunoRamos88 <88brunoramos@gmail.com>
2023-08-06 11:15:38 -0300

    Criação da page Movie

---
+++ src/mocks/Movie.ts
@@ -0,0 +1,16 @@
+import { IMovieRequestProps } from "../interfaces/Movie";
+
+export const movie: IMovieRequestProps = {
+  genres: [{ id: 18, name: 'Drama' }],
+  homepage: 'http://www.foxmovies.com/movies/fight-club',
+  id: 550,
+  overview:
+    'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground"fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+  poster_path: '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+  release_date: '1999-10-15',
+  revenue: 100853753,
+  runtime: 139,
+  title: 'Fight Club',
+  vote_average: 8.432,
+  vote_count: 24722,
+};
