06598e269555838b4b782a31dd2dde7fb2571ebd
BrunoRamos88 <88brunoramos@gmail.com>
2023-08-06 11:22:37 -0300

    Criação da page WishList

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../components/Header";
 import { Home } from "../pages/Home";
 import { Search } from "../pages/Search";
 import { Movie } from "../pages/Movie";
+import { WishList } from "../pages/WishList";
 
 export function AppRoutes() {
     return (
@@ -13,6 +14,7 @@ export function AppRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/movie" element={<Movie />} />
+                <Route path="/wishlist" element={<WishList />} />
             </Routes>
         </BrowserRouter>
     )
